a805387db313caf2df04e82034f567bbe3bfa0fb
yuslindanehe <yuslindanehe@mail.usf.edu>
2020-05-06 14:57:33 -0400

    change http to https

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -6,7 +6,7 @@
         Hi {{ $patient->firstName }},
         Thank you for using MyRecord. Please click link below to verify your email
 
-        <a href="http://yuzzapp.com?code={{ $patient->code }}&email={{ $patient->emailAddress }}">YuzzApp.com</a>
+        <a href="https://yuzzapp.com?code={{ $patient->code }}&email={{ $patient->emailAddress }}">YuzzApp.com</a>
 
         If you did not sign up for a MyRecord account please disregard this email.
 
